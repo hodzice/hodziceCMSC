4e9e42fc32c225f68a169a4e5bc67715cc7f32d6
hodzice <59925657+hodzice@users.noreply.github.com>
2020-01-15 10:11:23 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/**********************************************************************
+ * Hello.java
+ **********************************************************************
+ * Emina Hodzic
+ * January 15 2020
+ * 255 003
+ *********************************************************************/
+
+ public class Hello {
+
+ 	public static void main(String[] args) {
+
+ 	 System.out.println("<Emina Hodzic>");
+ 	 System.out.println("<Hello.java>");
+ 	 System.out.println("<Lab one>");
+ 	 System.out.println("<1.8.0_242>");
+ 	 System.out.println("<255 003>");
+ 	 System.out.println();
+ 	 }
+ }
